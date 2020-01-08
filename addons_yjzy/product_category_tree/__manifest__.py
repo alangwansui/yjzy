4685d319a156765698d0ccd31fb74874386d301c
alangwansui <alangwansui@qq.com>
2020-01-08 18:07:23 +0800

    'xxx'

---
+++ addons_yjzy/product_category_tree/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "product category tree",
+    'summary': """
+        """,
+    'description': """
+    """,
+    'author': "普森",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['product'],
+    'data': [
+        "data/main.xml",
+    ],
+}
---
+++ addons_yjzy/product_category_tree/models/main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import logging
+from odoo import _, api, models
+
+_logger = logging.getLogger(__name__)
+
+
+@api.model
+def get_categories(self):
+    # dbdata = self.search([], order="parent_id")
+    # data = [{'id': d.id, 'pid': d.parent_id.id, 'name': d.name} for d in dbdata]
+    data = []
+    return {'do_flag': False,
+            'field': 'category field',
+            'title': _('my category'),
+            'data': data
+            }
+
+
+models.BaseModel.get_categories = get_categories
+
+
+class Product(models.Model):
+    _inherit = "product.template"
+
+    @api.model
+    def get_categories(self):
+        dbdata = self.env['product.category'].search([])
+        data = [{'id': d.id, 'pid': d.parent_id.id, 'name': d.name} for d in dbdata]
+        return {'do_flag': True,
+                'field': 'categ_id',
+                'title': '产品类别',
+                'data': data
+                }
