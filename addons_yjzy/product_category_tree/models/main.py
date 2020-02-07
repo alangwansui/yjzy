303d836a8190bda6aedbe48f09010e095db1bf9f
alangwansui <alangwansui@qq.com>
2020-02-07 18:50:55 +0800

    xxx

--- addons_yjzy/product_category_tree/models/main.py
+++ addons_yjzy/product_category_tree/models/main.py
@@ -58,7 +58,7 @@ class mail_message(models.Model):
         suppliers = self.env['res.partner'].search([('supplier', '=', True)])
         personals = self.env['personal.partner'].search([])
         #users = self.env['res.users'].search([])
-        users = self.env.user.sub_message_uids
+        users = self.env.user.sub_message_uids | self.env['res.users'].search([('leader_user_id','=', self._uid)])
 
         data += [{'id': d.id, 'pid': d.parent_id.id or 'customer', 'name': d.name, 'model': 'res.partner'} for d in customers]
         data += [{'id': d.id, 'pid': d.parent_id.id or 'supplier', 'name': d.name, 'model': 'res.partner'} for d in suppliers]
