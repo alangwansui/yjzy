49f736a7ebeb5910a0d8f2201aef2c17d940fe3b
alangwansui <alangwansui@qq.com>
2020-02-13 22:02:54 +0800

    xxx

--- addons_yjzy/web_tree_image_tooltip/static/src/js/tooltip.js
+++ addons_yjzy/web_tree_image_tooltip/static/src/js/tooltip.js
@@ -3,6 +3,12 @@ odoo.define('web_tree_image_tooltip.web_tree_image_tooltip',
     "use strict";
 
     var ListRenderer = require('web.ListRenderer');
+
+
+    var screenX = 0;
+    var screenY = 0;
+
+
     ListRenderer.include({
         events: _.extend({}, ListRenderer.prototype.events, {
             'mouseover tbody tr td .tip_button': '_onHoverRecord_img',
@@ -12,12 +18,14 @@ odoo.define('web_tree_image_tooltip.web_tree_image_tooltip',
             //     $(event.currentTarget).children('.img-fluid').attr('src')
 
 
+            console.info('===xxyy==', event.screenX, screenX, event.screenY, screenY);
+
+            if (event.screenX != screenX || event.screenY != screenY){
+
+
 
             var rowIndex = event.fromElement.parentNode.rowIndex - 1;
-
-
-
-            console.info('===tooltipp==', rowIndex)
+            console.info('===tooltipp==', rowIndex, event);
 
 
             if(typeof rowIndex === 'number' && !isNaN(rowIndex)){
@@ -25,16 +33,18 @@ odoo.define('web_tree_image_tooltip.web_tree_image_tooltip',
                 if (line_data){
                     var body = line_data.data.body;
                     console.info('===', body, line_data);
-                    $(event.currentTarget).tooltip({
+                    var tip = $(event.currentTarget).tooltip({
                         html: true,
                         title: body,
-                        delay: 1,
-                        template: '<div class="tooltip"  role="tooltip"><div class="tooltip-arrow"></div><div  style="max-width: 200px !important;max-height: 200px !important"  class="tooltip-inner"></div></div>'
+                        delay: 20,
+                        template: '<div class="tooltip" style="border:2px solid #F00 !important; width: 200px !important;height: 200px !important;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 7; overflow: hidden;"  role="tooltip"><div class="tooltip-arrow"></div><div    class="tooltip-inner"></div></div>'
                     }).tooltip('show');
-
                     console.info('==tooltip ok=');
-
                 }
+            }
+
+            screenX = event.screenX;
+            screenY = event.screenY;
 
 
             }
