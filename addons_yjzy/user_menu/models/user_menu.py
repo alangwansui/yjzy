70c220d9e3bc17da6357e25bd6d16c3c84906309
alangwansui <alangwansui@qq.com>
2020-02-23 13:38:54 +0800

    'xxx'

--- addons_yjzy/user_menu/models/user_menu.py
+++ addons_yjzy/user_menu/models/user_menu.py
@@ -3,33 +3,24 @@ from odoo.tools.safe_eval import safe_eval
 from odoo import models, fields
 
 
+
 class user_menu(models.Model):
     _name = 'user.menu'
-    _description = '用户看板菜单定义'
+    _inherit = 'ir.actions.act_window'
+    _description = '工作看板'
 
-    name = fields.Char(u'名称', related='menu_id.name')
+    name = fields.Char(u'名称', required=True)
     user_id = fields.Many2one('res.users', u'用户')
-    menu_id = fields.Many2one('ir.ui.menu', '菜单')
-
     len_records = fields.Integer('数量', compute='compute_len_records')
 
     def compute_len_records(self):
+        gb_var = {'uid': self._uid}
         for one in self:
-            print(one.menu_id.action.domain)
-            model = one.menu_id.action.res_model
-            domian = safe_eval(one.menu_id.action.domain, {'uid': one._uid})
+            domain = safe_eval(one.domain or '[]', gb_var)
+            one.len_records = one.env[one.res_model].search_count(domain)
 
-
-            one.len_records = len(one.env[model].search_read(domian, ['id']))
-
-            print('===',  domian, one.len_records)
-
-
-
-
-
-    def open_menu(self):
-        action = self.menu_id.action.read()[0]
+    def open_action(self):
+        action = self.read()[0]
         return action
 
 
