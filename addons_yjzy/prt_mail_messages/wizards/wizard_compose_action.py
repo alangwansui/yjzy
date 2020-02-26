8908f3505cf071e5bd1aaed8cebf2a50edb614ff
alangwansui <alangwansui@qq.com>
2020-02-26 12:52:03 +0800

    'xxx'

--- addons_yjzy/prt_mail_messages/wizards/wizard_compose_action.py
+++ addons_yjzy/prt_mail_messages/wizards/wizard_compose_action.py
@@ -6,15 +6,15 @@ class wizard_compose_action(models.TransientModel):
 
     compose_id = fields.Many2one('mail.compose.message', u'撰稿')
 
-
     def open_message_out(self):
+        tree_view = self.env.ref('prt_mail_messages.prt_mail_message_out_tree')
+        form_view = self.env.ref('prt_mail_messages.prt_mail_message_out_tree')
         return {
             'name': '发件箱',
             'view_type': 'form',
             'view_mode': 'tree,form',
             'res_model': 'mail.message',
             'type': 'ir.actions.act_window',
-            'domain': [('message_type', '=', 'email'),('compose_id','=', self.compose_id.id)],
+            'views': [(tree_view.id, 'tree'), (form_view.id, 'form')],
+            'domain': [('message_type', '=', 'email'), ('compose_id', '=', self.compose_id.id)],
         }
-
-
