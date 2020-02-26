5886835d3edfe944f97eb42d7dcb41e34a130efd
alangwansui <alangwansui@qq.com>
2020-02-26 15:31:25 +0800

    'xxx'

--- odoo/addons/web/static/src/js/fields/relational_fields.js
+++ odoo/addons/web/static/src/js/fields/relational_fields.js
@@ -429,8 +429,7 @@ var FieldMany2One = AbstractField.extend({
                 var raw_result = _.map(result, function (x) { return x[1]; });
                 //<jon>
                 //if (create_enabled && !self.nodeOptions.no_quick_create &&
-                if (create_enabled && self.nodeOptions.quick_create &&
-                    search_val.length > 0 && !_.contains(raw_result, search_val)) {
+                if (self.nodeOptions.quick_create &&  search_val.length > 0 && !_.contains(raw_result, search_val)) {
                     values.push({
                         label: _.str.sprintf(_t('Create "<strong>%s</strong>"'),
                             $('<span />').text(search_val).html()),
