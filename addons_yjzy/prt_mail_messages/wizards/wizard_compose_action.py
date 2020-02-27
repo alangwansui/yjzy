297963309bc4a80594b2727f430e800a903f4eca
alangwansui <alangwansui@qq.com>
2020-02-27 09:50:04 +0800

    'xxx'

--- addons_yjzy/prt_mail_messages/wizards/wizard_compose_action.py
+++ addons_yjzy/prt_mail_messages/wizards/wizard_compose_action.py
@@ -8,7 +8,7 @@ class wizard_compose_action(models.TransientModel):
 
     def open_message_out(self):
         tree_view = self.env.ref('prt_mail_messages.prt_mail_message_out_tree')
-        form_view = self.env.ref('prt_mail_messages.prt_mail_message_out_tree')
+        form_view = self.env.ref('prt_mail_messages.prt_mail_message_out_form')
         return {
             'name': '发件箱',
             'view_type': 'form',
