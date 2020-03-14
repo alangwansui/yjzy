0bf3beca619df9eeeb293fee6a382afc638ea996
akiny <2277982220@qq.com>
2020-03-14 10:12:03 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/hr_expense.py

--- odoo/http.py
+++ odoo/http.py
@@ -1439,7 +1439,7 @@ class Root(object):
         # - It could allow session fixation attacks.
         if not explicit_session and hasattr(response, 'set_cookie'):
             response.set_cookie(
-                'session_id', httprequest.session.sid, max_age=50 * 60 * 60, httponly=True)
+                'session_id', httprequest.session.sid, max_age=3 * 60 * 60, httponly=True)
 
         return response
 
