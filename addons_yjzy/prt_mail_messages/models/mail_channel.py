c01cb8baa29cf1c590552f241c28ee8d5e25e5f2
alangwansui <alangwansui@qq.com>
2020-03-17 11:25:39 +0800

    'xxx'

--- addons_yjzy/prt_mail_messages/models/mail_channel.py
+++ addons_yjzy/prt_mail_messages/models/mail_channel.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, tools, _
-
+from odoo import api, fields, models, tools, _, SUPERUSER_ID
+from odoo.exceptions import Warning
 
 class mail_channel(models.Model):
     _inherit = 'mail.channel'
 
     sent_uid = fields.Many2one('res.users', u'发件所有者')
     chat_uid = fields.Many2one('res.users',  '系统通知用户')
+
+    @api.multi
+    def action_unfollow(self):
+        raise Warning('不允许退出频道')
+        #return self._action_unfollow(self.env.user.partner_id)
 
 
 
