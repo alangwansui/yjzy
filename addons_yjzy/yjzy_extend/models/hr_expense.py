7e2808a3a817fe05304444e0d3ccb3c58546f9dc
akiny <2277982220@qq.com>
2020-03-22 15:38:45 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/hr_expense.py
    #	addons_yjzy/yjzy_extend/views/hr_expense_new.xml

--- addons_yjzy/yjzy_extend/models/hr_expense.py
+++ addons_yjzy/yjzy_extend/models/hr_expense.py
@@ -101,21 +101,21 @@ class hr_expense_sheet(models.Model):
     expense_line_ids_company = fields.One2many('hr.expense', related='expense_line_ids')
 
 
-   # @api.onchange('categ_id')
-   # def onchange_categ(self):
-   #     self.second_categ_id = None
-   #     for line in self.expense_line_ids:
-   #         line.categ_id = self.categ_id
-   #         line.second_categ_id = None
-   #         line.product_id = False
+    @api.onchange('categ_id')
+    def onchange_categ(self):
+        self.second_categ_id = None
+        for line in self.expense_line_ids:
+            line.categ_id = self.categ_id
+            line.second_categ_id = None
+            line.product_id = False
 
 
-   # @api.onchange('second_categ_id')
-  #  def onchange_second_categ(self):
-    #    for line in self.my_expense_line_ids:
-    #        line.categ_id = self.categ_id
-    #        line.second_categ_id = self.second_categ_id
-   #         line.product_id = False
+    @api.onchange('second_categ_id')
+    def onchange_second_categ(self):
+        for line in self.my_expense_line_ids:
+            line.categ_id = self.categ_id
+            line.second_categ_id = self.second_categ_id
+            line.product_id = False
 
     @api.one
     def compute_total_this_year(self):
