9ee60e8f873b621d97003bfd248a560ac6ecf149
alangwansui <alangwansui@qq.com>
2020-04-01 23:53:57 +0800

    'xxx'

--- addons_yjzy/yjzy_extend/wizard/wizard_transport_lot_plan.py
+++ addons_yjzy/yjzy_extend/wizard/wizard_transport_lot_plan.py
@@ -14,14 +14,25 @@ class wizard_transport_lot_plan(models.TransientModel):
     def apply(self):
         self.ensure_one()
         plan_obj = self.env['transport.lot.plan']
+
+        i = 1
         for line in self.line_ids:
             data = {'lot_id': line.lot_id.id, 'qty': line.qty, 'stage_1': line.stage_1, 'stage_2': line.stage_2}
             plan = line.plan_id
+
+
+
             if plan:
                 plan.write(data)
             else:
                 data.update({'tbline_id': self.tbline_id.id})
                 plan = plan_obj.create(data)
+
+            #明细的计划永远未第一条计划
+            if i == 1:
+                line.tbline_id.lot_plan_id = plan
+            i += 1
+
         return True
 
 
@@ -32,8 +43,8 @@ class wizard_transport_lot_plan_line(models.TransientModel):
     plan_id = fields.Many2one('transport.lot.plan')
     lot_id = fields.Many2one('stock.production.lot', u'批次')
     qty = fields.Float('数量')
-    stage_1 = fields.Boolean('应用第1步')
-    stage_2 = fields.Boolean('应用第2步')
+    stage_1 = fields.Boolean('应用第1步', defualt=True)
+    stage_2 = fields.Boolean('应用第2步', defualt=True)
 
     def unlink_plan(self):
         self.ensure_one()
