e5d81d294704b009a0e51b6dda647c1b1e9817d3
alangwansui <alangwansui@qq.com>
2020-04-01 14:43:02 +0800

    'xxx'

--- addons_yjzy/yjzy_extend/models/employee.py
+++ addons_yjzy/yjzy_extend/models/employee.py
@@ -6,8 +6,8 @@ from odoo.exceptions import Warning
 class hr_employee(models.Model):
     _inherit = 'hr.employee'
 
-    def write(self, vals):
-        if 'user_id' in vals:
-            raise Warning('xxxxxxxxxxx')
-        else:
-            return super(hr_employee, self).write(vals)
+    # def write(self, vals):
+    #     if 'user_id' in vals:
+    #         raise Warning('xxxxxxxxxxx')
+    #     else:
+    #         return super(hr_employee, self).write(vals)
