e40165e3df689663ac8317a84c88127607b44352
alangwansui <alangwansui@qq.com>
2020-04-10 14:33:01 +0800

    'xxx'

---
+++ addons_yjzy/yjzy_extend/models/attachment.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
+
+
+class IrAttachment(models.Model):
+    _inherit = 'ir.attachment'
+
+    def unlink(self):
+        for one in self:
+
+
+
+            if one.res_model and one.res_id:
+                record = self.env[one.res_model].browse(one.res_id)
+
+                print('==', record, record.state, record._name, record.name == 'sale.order', record.state in ['sale', 'done'])
+
+                if record._name == 'sale.order':
+                    if record.state in ['sale', 'done']:
+                        raise Warning('确认的销售订单禁止删除附件')
+                elif record.name == 'purchase.order':
+                    pass
+
+
+        return super(IrAttachment, self).unlink()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
