beb2f27f2ad2cbd9da0c81188349fe087180da12
akiny <2277982220@qq.com>
2020-04-14 22:48:17 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/sr_sale_multi_product_selection/views/sale.xml

--- addons_yjzy/yjzy_extend/models/sale_line.py
+++ addons_yjzy/yjzy_extend/models/sale_line.py
@@ -153,8 +153,8 @@ class sale_order_line(models.Model):
         self.back_tax = self.product_id.back_tax
         #self.s_uom_id = self.product_id.uom_id
         #self.p_uom_id = self.product_id.uom_po_id
-         self.s_uom_id = self.product_id.s_uom_id
-         self.p_uom_id = self.product_id.p_uom_id
+        self.s_uom_id = self.product_id.s_uom_id
+        self.p_uom_id = self.product_id.p_uom_id
         self.need_split_bom = self.product_id.need_split_bom
         self.need_print = self.product_id.need_print
         return res
