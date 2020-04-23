6a3360848ad429f373b1325cf43b34fe253305e3
akiny <2277982220@qq.com>
2020-04-23 17:56:57 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/sr_sale_multi_product_selection/views/sale.xml

--- addons_yjzy/yjzy_extend/models/transport_bill.py
+++ addons_yjzy/yjzy_extend/models/transport_bill.py
@@ -634,9 +634,9 @@ class transport_bill(models.Model):
         self.partner_country_id = self.partner_id.country_id
         #akiny
        # self.user_id = self.partner_id.user_id
-        #self.sale_currency_id = self.partner_id.property_product_pricelist.currency_id
+        self.sale_currency_id = self.partner_id.property_product_pricelist.currency_id
 
-        self.mark_html = self.partner_shipping_id.mark_html.currency_id
+     #   self.mark_html = self.partner_shipping_id.mark_html.currency_id
        # akiny
         ##self.outer_currency_id = self.sale_currency_id
         self.notice_man = self.partner_id.notice_man
