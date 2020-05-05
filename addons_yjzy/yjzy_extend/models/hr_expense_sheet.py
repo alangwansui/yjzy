094aff67200d77edc7c86e4e5f7a9bea719be349
akiny <2277982220@qq.com>
2020-05-05 22:35:34 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/hr_expense.py

--- addons_yjzy/yjzy_extend/models/hr_expense_sheet.py
+++ addons_yjzy/yjzy_extend/models/hr_expense_sheet.py
@@ -177,8 +177,9 @@ class hr_expense_sheet(models.Model):
 
 
         sql = """select sum(company_currency_total_amount) from hr_expense where state in ('done') and total_amount > 0 and (categ_id != 193 or categ_id is null) and payment_date > '%s' """
-        sql_approve = """select sum(company_currency_total_amount) from hr_expense where state in ('approve') and (categ_id != 193 or categ_id is null ) and total_amount > 0 """
-        sql_submit = """select sum(company_currency_total_amount) from hr_expense where state in ('reported','employee_confirm','confirmed','manager_approval') and (categ_id != 193 or categ_id is null) and total_amount > 0 """
+        sql_approve = """select sum(company_currency_total_amount) from hr_expense where state in ('confirmed') and (categ_id != 193 or categ_id is null ) and total_amount > 0 """
+        sql_submit = """select sum(company_currency_total_amount) from hr_expense where state in ('reported','employee_confirm') and (categ_id != 193 or categ_id is null) and total_amount > 0 """
+
 
         moth_sql = sql % month
         year_sql = sql % year
