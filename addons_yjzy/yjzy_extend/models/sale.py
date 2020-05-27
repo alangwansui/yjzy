9178212d779940c39820a97ffe94e863c558258c
akiny <2277982220@qq.com>
2020-05-28 00:12:42 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/sale.py

--- addons_yjzy/yjzy_extend/models/sale.py
+++ addons_yjzy/yjzy_extend/models/sale.py
@@ -452,7 +452,8 @@ class sale_order(models.Model):
 
     is_different_payment_term = fields.Boolean('付款条款是否不同')
 
-    hexiao_type = fields.Selection([('abnormal',u'异常核销'),('write_off',u'正常核销')], string='核销状态')
+    hexiao_type = fields.Selection([('abnormal',u'异常核销'),('write_off',u'正常核销')], string='核销类型')
+
     hexiao_comment = fields.Text(u'异常核销备注')
     doing_type = fields.Selection([('undelivered', u'未发货'), ('start_delivery', u'开始发货'),
                                    ('wait_hexiao',u'待核销'),('has_hexiao',u'已核销')],
@@ -796,7 +797,7 @@ class sale_order(models.Model):
                 state='verifying'
                 hexiao_type = 'abnormal'
             if one.delivery_status == 'received' and one.state != 'verification':
-                if one.balance == 0 and one.purchase_balance_sum == 0:
+                if one.balance == 0 and one.purchase_balance_sum3 == 0:
                     hexiao_type = 'write_off'
                     state = 'verifying'
                 else:
