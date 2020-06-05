e5e05644cc3601ca42049be863b4637893a34a06
akiny <2277982220@qq.com>
2020-06-05 14:10:18 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/sale.py

--- addons_yjzy/yjzy_extend/models/trans_date_attachment.py
+++ addons_yjzy/yjzy_extend/models/trans_date_attachment.py
@@ -9,7 +9,7 @@ class trans_date_attachment(models.Model):
     _description = '附件管理'
 
     name = fields.Char('编号', default=lambda self: self.env['ir.sequence'].next_by_code('trans.date.attachment'))
-    attachment = fields.Many2many('ir.attachment', string='进仓日附件')
+    attachment = fields.Many2many('ir.attachment', string='附件')
     type = fields.Selection([('date_out_in', u'1'),
                                ('date_ship', u'2'),
                                ('date_customer_finish', u'3'),
