71617b7dd58e14a635f053ce8a2f6b50cd4e3cac
akiny <2277982220@qq.com>
2020-06-13 10:18:18 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/sale.py

--- addons_yjzy/yjzy_extend/models/partner.py
+++ addons_yjzy/yjzy_extend/models/partner.py
@@ -40,7 +40,7 @@ class res_partner(models.Model):
         company = self.env.user.company_id.id
         for one in self:
             if one.sale_order_ids:
-                last_sale_orders = one.sale_order_ids.filtered(lambda x: x.approve_date != False and x.company_id == company)
+                last_sale_orders = one.sudo().sale_order_ids.filtered(lambda x: x.approve_date != False and x.company_id == company)
                 last_order = last_sale_orders[0]
                 one.last_sale_order_approve_date = last_order.approve_date
             # if one.sale_order_ids:
