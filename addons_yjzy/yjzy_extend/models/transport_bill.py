2fd6b497e65367cbe5fd04f4d1372e37d0e3aa1e
akiny <2277982220@qq.com>
2020-06-16 21:32:04 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/sale.py

--- addons_yjzy/yjzy_extend/models/transport_bill.py
+++ addons_yjzy/yjzy_extend/models/transport_bill.py
@@ -225,7 +225,7 @@ class transport_bill(models.Model):
 
             one.all_purchase_invoice_fill = all([x.date_finish for x in purchase_invoices])
 
-    @api.depends('line_ids.plan_qty')
+    @api.depends('line_ids.plan_qty','line_ids','current_date_rate')
     def _amount_all(self):
         """
         Compute the total amounts of the SO.
