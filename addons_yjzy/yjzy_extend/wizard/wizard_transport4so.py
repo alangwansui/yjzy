e8392f6d8634098ed3fd1103e1fd2e64798b32d2
akiny <2277982220@qq.com>
2020-06-18 08:20:47 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/sale.py

--- addons_yjzy/yjzy_extend/wizard/wizard_transport4so.py
+++ addons_yjzy/yjzy_extend/wizard/wizard_transport4so.py
@@ -91,7 +91,7 @@ class wizard_transport4so(models.TransientModel):
         tb.sale_currency_id = self.currency_id
         tb.include_tax = self.include_tax
         tblines.make_default_lot_plan()
-        tblines.bill_id.compute_same()
+        tb.compute_same()
         tb.is_done_plan = True
         return True
 
@@ -126,7 +126,7 @@ class wizard_transport4so(models.TransientModel):
         tb.check_lines()
         tblines.make_default_lot_plan()
         tb.is_done_plan = True
-        tblines.bill_id.compute_same()
+        tb.compute_same()
         return True
 
     def apply(self):
