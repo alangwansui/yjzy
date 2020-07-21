e208ec91d7accea84c06b4e1ad26ac8342a62821
akiny <2277982220@qq.com>
2020-07-21 23:51:52 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	addons_yjzy/yjzy_extend/models/sale.py

--- addons_yjzy/yjzy_extend/models/partner.py
+++ addons_yjzy/yjzy_extend/models/partner.py
@@ -147,9 +147,9 @@ class res_partner(models.Model):
     so_approve_ids = fields.One2many('sale.order','partner_id','今年销售合同',
                                      domain=[('approve_date','!=',False),('approve_date','>',fields.datetime.now().strftime('%Y-01-01 00:00:00')),('state','in',['approve', 'sale', 'done','abnormal','verifying','verification'])])
     so_no_sent_amount = fields.Float('未发货余额', compute=compute_sale_order_amount_total,store=True)
-    account_reconcile_ids = fields.One2many('account.reconcile.order','partner_id','应收认领', domain=[('sfk_type','=','yshxd'),('amount_payment_org','!=',0)])
+    account_reconcile_ids = fields.One2many('account.reconcile.order','partner_id','应收认领', domain=[('sfk_type','=','yshxd'),('state','=','done'),('amount_payment_org','!=',0)])
     supplier_account_reconcile_ids = fields.One2many('account.reconcile.order', 'partner_id', '应付认领',
-                                            domain=[('sfk_type', '=', 'yfhxd'), ('amount_payment_org', '!=', 0)])
+                                            domain=[('sfk_type', '=', 'yfhxd'),('state','=','done'), ('amount_payment_org', '!=', 0)])
     amount_invoice = fields.Float(u'应收账单总金额', compute=compute_amount_invoice_advance_payment,store=True)
     amount_residual_invoice = fields.Float(u'应收款余额',compute=compute_amount_invoice_advance_payment,store=True)
     amount_advance_payment = fields.Float('u预收总金额',compute=compute_amount_invoice_advance_payment,store=True)
