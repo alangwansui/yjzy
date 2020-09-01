634ff57d15c6abc4cf65fc40186e1aba0e81d7b4
akiny <2277982220@qq.com>
2020-09-02 00:10:36 +0800

    Merge branch 'master' of https://gitee.com/alangwansui/yjzy
    # Conflicts:
    #	odoo/addons/mail/models/mail_thread.py

--- addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
+++ addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
@@ -150,7 +150,8 @@ class account_reconcile_order(models.Model):
             return None
 
     def compute_approve_date_uid(self):
-        message_id = self.message_ids[-1]
+        # message_id = self.message_ids[-1]
+        message_id = self.message_ids.filtered(lambda x: x.author_id.name == '奚海峰')
         approve_uid = message_id.author_id.x_studio_field_REAqh
         approve_date =message_id.date
         print('message_id',message_id,approve_uid,approve_date)
