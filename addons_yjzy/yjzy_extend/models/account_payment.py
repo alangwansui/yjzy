f00b2a81a8afec27e8c366e2102e80008e84a825
akiny <2277982220@qq.com>
2020-09-07 22:32:16 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/account_payment.py
+++ addons_yjzy/yjzy_extend/models/account_payment.py
@@ -188,7 +188,17 @@ class account_payment(models.Model):
             elif ctx.get('bank_amount'):
                 name = '%s[%s]' % (one.journal_id.name, str(one.balance))
             elif ctx.get('advance_bank_amount'):
-                name = '%s[%s]' % (one.yjzy_payment_id.journal_id.name, str(one.balance))
+                name = '%s[%s]' % (one.yjzy_payment_id.journal_id.name, str(one.advance_balance_total))
+            elif ctx.get('advance_so_amount'):
+                if one.yjzy_payment_id.so_id:
+                    name = '%s[%s]' % (one.yjzy_payment_id.so_id, str(one.advance_balance_total))
+                else:
+                    name= '%s[%s]' % ('无销售合同', str(one.advance_balance_total))
+            elif ctx.get('advance_po_amount'):
+                if one.yjzy_payment_id.po_id:
+                    name = '%s[%s]' % (one.yjzy_payment_id.po_id, str(one.advance_balance_total))
+                else:
+                    name= '%s[%s]' % ('无采购合同', str(one.advance_balance_total))
             else:
                 name = '%s[%s]' % (one.name, str(one.balance))
             one.display_name = name
