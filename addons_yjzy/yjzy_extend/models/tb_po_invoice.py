0b905d55bba3ac1c58c98242e8e8e8bd2cb182d7
akiny <2277982220@qq.com>
2020-09-14 19:28:27 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/tb_po_invoice.py
+++ addons_yjzy/yjzy_extend/models/tb_po_invoice.py
@@ -183,7 +183,7 @@ class tb_po_invoice(models.Model):
     price_total = fields.Monetary('金额合计',currency_field='currency_id',compute=compute_info,store=True)
 
 
-    state = fields.Selection([('10_draft',u'草稿'),('20_submit',u'已提交'),('30_done','审批完成'),('40_refuse',u'拒绝')],u'状态',index=True, track_visibility='onchange', default='10_draft')
+    state = fields.Selection([('10_draft',u'草稿'),('20_submit',u'已提交'),('30_done','审批完成'),('80_refuse',u'拒绝'),('90_cancel',u'取消')],u'状态',index=True, track_visibility='onchange', default='10_draft')
     type = fields.Selection([('extra', '额外账单'), ('other_po', '直接增加'),('expense_po', u'费用转换')],u'类型')
     name = fields.Char('编号', default=lambda self: self.env['ir.sequence'].next_by_code('tb.po.invoice'))
     tb_id = fields.Many2one('transport.bill', u'出运单')
@@ -252,7 +252,7 @@ class tb_po_invoice(models.Model):
         #self.invoice_ids.action_invoice_open()
     def action_refuse(self):
         self.invoice_ids.unlink()
-        self.state = '40_refuse'
+        self.state = '80_refuse'
     def action_draft(self):
         self.state = '10_draft'
 
