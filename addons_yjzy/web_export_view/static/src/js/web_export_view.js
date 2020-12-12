0fd27cd15a1c9e7b8258ed57b8ca31e98ba003dc
akiny <2277982220@qq.com>
2020-12-12 15:13:17 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/web_export_view/static/src/js/web_export_view.js
+++ addons_yjzy/web_export_view/static/src/js/web_export_view.js
@@ -19,7 +19,7 @@ odoo.define('web_export_view', function (require) {
                 session.user_has_group(
                     'web_export_view.group_disallow_export_view_data_excel')
                     .then(function (has_group) {
-                        if (has_group) {
+                        if (has_group) {  //!has_group 改为has_group, 群组名称没改不要弄错
                             self.$el.find('.o_dropdown')
                                 .last().append(QWeb.render(
                                     'WebExportTreeViewXls', {widget: self}));
