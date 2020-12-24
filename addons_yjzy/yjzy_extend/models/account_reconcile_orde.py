cf544f33ef89b6c9573ee71d27625106b48feaac
akiny <2277982220@qq.com>
2020-12-24 23:24:02 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
+++ addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
@@ -443,7 +443,13 @@ class account_reconcile_order(models.Model):
                 one.partner_payment_term_id = one.partner_id.property_supplier_payment_term_id
             one.invoice_payment_term_id = one.invoice_ids[0].payment_term_id
 
+    def compute_invoice_id(self):
+        for one in self:
+            one.invoice_id = one.invoice_ids[0]
 
+
+    invoice_id = fields.Many2one('account.invoice',compute=compute_invoice_id)
+    invoice_attribute_all_in_one = fields.Char('账单属性all_in_one',realated='invoice_id.invoice_attribute_all_in_one')
 
     partner_payment_term_id = fields.Many2one('account.payment.term',u'伙伴付款条款',compute=compute_payment_term_id)
     invoice_payment_term_id = fields.Many2one('account.payment.term',u'合同付款条款',compute=compute_payment_term_id)
