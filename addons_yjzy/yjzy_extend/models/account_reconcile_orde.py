d8ab8a636634b0497f12f069cffcabd8b2c1cb07
akiny <2277982220@qq.com>
2021-01-19 13:37:14 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
+++ addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
@@ -580,16 +580,16 @@ class account_reconcile_order(models.Model):
     name_title = fields.Char(u'账单描述')
     invoice_partner = fields.Char(u'账单对象')
 
-    supplier_advance_payment_ids_amount_advance_org = fields.Float('待审批预付认领金额',compute= compute_supplier_advance_payment_ids_amount_advance_org)
-    supplier_amount_invoice_approval = fields.Float(u'已申请未审批',compute=compute_supplier_amount_invoice_approve_approval)
-    supplier_amount_invoice_approve = fields.Float(u'已审批未付款',compute=compute_supplier_amount_invoice_approve_approval)
-    supplier_amount_invoice_residual = fields.Float(u'供应商应付余额',compute=compute_supplier_amount_invoice_approve_approval)
+    supplier_advance_payment_ids_amount_advance_org = fields.Float('待审批预付认领金额',)#compute= compute_supplier_advance_payment_ids_amount_advance_org
+    supplier_amount_invoice_approval = fields.Float(u'已申请未审批',)#compute=compute_supplier_amount_invoice_approve_approval
+    supplier_amount_invoice_approve = fields.Float(u'已审批未付款',)#compute=compute_supplier_amount_invoice_approve_approval
+    supplier_amount_invoice_residual = fields.Float(u'供应商应付余额',)#compute=compute_supplier_amount_invoice_approve_approval
     supplier_amount_invoice_residual2 = fields.Float(u'供应商应付余额2',
-                                                    compute=compute_supplier_amount_invoice_approve_approval,
-                                                    )
 
-    supplier_advance_amount_hxd_line_approval= fields.Float(u'供应商预付审批中预付认领',compute=compute_supplier_amount_invoice_approve_approval)
-    supplier_advance_amount_hxd_line_approval2 = fields.Float(u'供应商预付余额2',compute=compute_supplier_amount_invoice_approve_approval)
+                                                    )#compute=compute_supplier_amount_invoice_approve_approval
+
+    supplier_advance_amount_hxd_line_approval= fields.Float(u'供应商预付审批中预付认领',)#compute=compute_supplier_amount_invoice_approve_approval
+    supplier_advance_amount_hxd_line_approval2 = fields.Float(u'供应商预付余额2',)#compute=compute_supplier_amount_invoice_approve_approval
     yjzy_type = fields.Selection([('sale','销售'),
                                   ('purchase','采购'),
                                   ('back_tax','退税'),
@@ -665,8 +665,8 @@ class account_reconcile_order(models.Model):
 
     amount_purchase_advance = fields.Monetary('预付金额:本币', currency_field='currency_id',related='partner_id.amount_purchase_advance')
     currency_id = fields.Many2one(related='company_id.currency_id', string=u'公司货币', store=True, index=True)
-    supplier_amount_advance_payment = fields.Float('u预付总金额', compute=compute_supplier_amount_residual_advance_payment)
-    supplier_amount_residual_advance_payment = fields.Float('预付余额', compute=compute_supplier_amount_residual_advance_payment)
+    supplier_amount_advance_payment = fields.Float('u预付总金额', )#compute=compute_supplier_amount_residual_advance_payment
+    supplier_amount_residual_advance_payment = fields.Float('预付余额', )#compute=compute_supplier_amount_residual_advance_payment
 
     invoice_currency_id = fields.Many2one('res.currency', u'交易货币', compute=compute_by_invoice)
     state = fields.Selection([('draft', u'草稿'),
@@ -3635,7 +3635,7 @@ class account_reconcile_order_line(models.Model):
     amount_exchange = fields.Monetary(u'汇兑差异:本币', currency_field='currency_id')
     amount_total_org = fields.Monetary(u'收款合计', currency_field='invoice_currency_id', compute=compute_info)
     amount_total = fields.Monetary(u'收款合计:本币', currency_field='currency_id', compute=compute_info)
-    amount_total_org_new = fields.Monetary(u'收款合计', currency_field='invoice_currency_id', store=True)#compute=compute_amount_total_org_new,
+    amount_total_org_new = fields.Monetary(u'收款合计', currency_field='invoice_currency_id', compute=compute_amount_total_org_new,store=True)#在这里
 
     @api.onchange('yjzy_payment_id')
     def onchange_yjzy_payment_id(self):
