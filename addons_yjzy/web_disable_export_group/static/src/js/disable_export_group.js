f126161b26805cf1d297c3361f62755aab1871b7
akiny <2277982220@qq.com>
2021-01-24 19:28:35 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/web_disable_export_group/static/src/js/disable_export_group.js
+++ addons_yjzy/web_disable_export_group/static/src/js/disable_export_group.js
@@ -41,13 +41,20 @@ odoo.define("web_disable_export_group", function(require) {
                 _items = _.reject(_items, {label: "Move message"});
 
             };
+
+            var move_action_id = 1443; //数字为需要显示的actionid, 可以通过‘菜单’ 查询得到
+            if(menu_action_id != move_action_id){
+                _items = _.reject(_items, {label: "创建付款指令"});
+            };
+
+
 //            var cjyfsq_action_id = 1335; //数字为需要显示的actionid, 可以通过‘菜单’ 查询得到
 //            if(menu_action_id != cjyfsq_action_id){
 //                _items = _.reject(_items, {label: "创建应付申请"});
 //
 //            };
 
-            var cjyfsq_action_id = [1386,1387,1385]; //数字为需要显示的actionid, 可以通过‘菜单’ 查询得到
+            var cjyfsq_action_id = [1386,1387,1385,1405,1406,1407]; //数字为需要显示的actionid, 可以通过‘菜单’ 查询得到
             if (cjyfsq_action_id.indexOf(menu_action_id) == -1){
                 _items = _.reject(_items, {label: "创建应付申请"});
 
