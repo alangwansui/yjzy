fb98e6d767bbe071302a79544f2f4e3bf8d93faf
akiny <2277982220@qq.com>
2021-01-28 20:50:55 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
+++ addons_yjzy/yjzy_extend/models/account_reconcile_orde.py
@@ -4088,8 +4088,8 @@ class advance_payment_state(models.Model):
         self.ensure_one()
         dic_so_invl = {}
         for line in inv.invoice_line_ids:
-            if line.sale_line_ids: #or line.manual_so_id:
-                so = line.sale_line_ids[0].order_id #or line.manual_so_id
+            if line.sale_line_ids or line.manual_so_id:
+                so = line.sale_line_ids[0].order_id or line.manual_so_id
                 if so in dic_so_invl:
                     dic_so_invl[so] |= line
                 else:
@@ -4100,8 +4100,8 @@ class advance_payment_state(models.Model):
         self.ensure_one()
         dic_po_invl = {}
         for line in inv.invoice_line_ids:
-            if line.purchase_id: #or line.manual_po_id:
-                po = line.purchase_id #or line.manual_po_id
+            if line.purchase_id or line.manual_po_id:
+                po = line.purchase_id or line.manual_po_id
                 if po in dic_po_invl:
                     dic_po_invl[po] |= line
                 else:
