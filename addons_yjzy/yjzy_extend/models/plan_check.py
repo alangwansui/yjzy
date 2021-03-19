7b8f3c4537aec8445ca33aa2b54a23812e1c87b8
akiny <2277982220@qq.com>
2021-03-19 11:37:52 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/plan_check.py
+++ addons_yjzy/yjzy_extend/models/plan_check.py
@@ -618,8 +618,8 @@ class PlanCheck(models.Model):
                 company_id = one.tb_id.company_id
             one.company_id = company_id
 
-
-    type = fields.Selection([('new_order_track', '新订单下单前跟踪'), ('order_track', '订单跟踪'), ('transport_track','出运单跟踪')], 'type')
+    type = fields.Selection([('new_order_track', '新订单下单前跟踪'), ('order_track', '订单跟踪'), ('transport_track', '出运单跟踪')],
+                            'type', related='order_track_id.type')
     display_name = fields.Char(u'显示名称', compute=compute_display_name)
     order_track_id = fields.Many2one('order.track','计划跟踪',ondelete='cascade')
     # plan_check_ids = fields.One2many('plan.check','so_id')
