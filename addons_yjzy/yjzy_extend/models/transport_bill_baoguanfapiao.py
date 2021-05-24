5abbbfe390bbcae234fcc99dea01597b7bd9268d
akiny <2277982220@qq.com>
2021-05-24 13:31:44 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/transport_bill_baoguanfapiao.py
+++ addons_yjzy/yjzy_extend/models/transport_bill_baoguanfapiao.py
@@ -33,6 +33,17 @@ class transport_bill(models.Model):
             # self.create_hsname_all_ids()
             # self.create_btls_hs_ids_purchase()
 
+    def action_005_stage(self):
+        stage_id = self._stage_find(domain=[('code', '=', '005')])
+        stage_preview = self.stage_id
+        user = self.env.user
+        if user not in stage_preview.user_ids:
+            raise Warning('您没有权限审批')
+        else:
+            self.write({'stage_id': stage_id.id})
+            # self.create_hsname_all_ids()
+            # self.create_btls_hs_ids_purchase()
+
 
     def action_finish_add_purchase_stage(self):
         stage_id = self._stage_find(domain=[('code', '=', '013')])
