e1aaaa2699cda9fba93ad8a413c65e551cd4cf89
akiny <2277982220@qq.com>
2021-06-23 21:42:43 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/real_invoice_auto.py
+++ addons_yjzy/yjzy_extend/models/real_invoice_auto.py
@@ -219,8 +219,9 @@ class PlanInvoiceAuto(models.Model):
                             one.state_2 = '75'
                             one.state_1 = '40'
                 elif one.state_1 == '50':
+                    print('invoice_akiny', len(back_tax_invoice_declare_ids), len(back_tax_invoice_ids))
                     if back_tax_invoice_declare_ids and back_tax_invoice_ids:
-                        print('invoice_akiny',len(back_tax_invoice_declare_ids),len(back_tax_invoice_ids))
+
                         if len(back_tax_invoice_declare_ids) != len(back_tax_invoice_ids):
                             if date_ship_residual_time >= 30:
                                 one.state_2 = '90'
