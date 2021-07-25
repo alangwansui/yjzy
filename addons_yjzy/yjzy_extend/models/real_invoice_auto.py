73b0ca6a4df852a6532d37855e841093234b235d
akiny <2277982220@qq.com>
2021-07-25 14:30:07 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/real_invoice_auto.py
+++ addons_yjzy/yjzy_extend/models/real_invoice_auto.py
@@ -140,11 +140,11 @@ class PlanInvoiceAuto(models.Model):
 
     state_1 = fields.Selection(
         [('10', '报关数据待锁定'), ('20', '报关数据已锁定-应收发票待锁定'), ('30', '应收发票锁定-发票未收齐'), ('40', '发票已收齐-未开销项'),
-         ('50', '已开销项-未申报退税'), ('60', '已申报退税未收退税'), ('70','已收退税')],'state_1',track_visibility='onchange',default='10')
+         ('50', '已开销项-未申报退税'), ('60', '已申报退税未收退税'), ('70','已收退税')],'state_1',index=True,track_visibility='onchange',default='10')
     state_2 = fields.Selection(
         [('10', '正常待锁定.'), ('20', '异常待锁定.'), ('30', '正常待锁定'), ('40', '异常待锁定'), ('50', '正常未收齐'),
          ('60', '异常未收齐'), ('70', '正常未开'), ('75', '异常未开'), ('80', '正常未申报'), ('90', '异常未申报'), ('100', '正常未收'),('110','异常未收'),('120','已收退税')],
-        'State', track_visibility='onchange', default='10')
+        'State', index=True,track_visibility='onchange', default='10')
 
     real_invoice_auto_id = fields.One2many('real.invoice.auto', 'plan_invoice_auto_id', '实际进项发票')
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True,
