cc2a2f12868fe0bef8c85e399fe17c59ef28d347
akiny <2277982220@qq.com>
2021-10-20 09:18:28 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/currency.py
+++ addons_yjzy/yjzy_extend/models/currency.py
@@ -10,3 +10,10 @@ class Currency(models.Model):
     _inherit = 'res.currency'
     rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=(12, 8),
                         help='The rate of the currency to the currency of rate 1.')
+
+
+class CurrencyRate(models.Model):
+    _inherit = "res.currency.rate"
+
+
+    rate = fields.Float(digits=(12, 8), help='The rate of the currency to the currency of rate 1')
