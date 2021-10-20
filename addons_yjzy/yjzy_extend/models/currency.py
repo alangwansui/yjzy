23dbcd2318038500d3bee806922c4ef58af6a5c0
akiny <2277982220@qq.com>
2021-10-20 09:15:46 +0800

    Revert "Merge branch 'master' of https://gitee.com/alangwansui/yjzy"
    This reverts commit a84d955b3d6e543dcd9cd39aeabbf247d8f325e0.

--- addons_yjzy/yjzy_extend/models/currency.py
+++ addons_yjzy/yjzy_extend/models/currency.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT as DF
+from datetime import datetime, timedelta, date
 
 
 
-
-#
+class Currency(models.Model):
+    _inherit = 'res.currency'
+    rate = fields.Float(compute='_compute_current_rate', string='Current Rate', digits=(12, 8),
+                        help='The rate of the currency to the currency of rate 1.')
