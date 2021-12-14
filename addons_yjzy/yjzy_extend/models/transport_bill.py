ef2af1afdd2b1b18d2a9be375b78777225518ad2
Rilay <2277982220@qq.com>
2021-12-14 14:53:54 +0800

    1024

--- addons_yjzy/yjzy_extend/models/transport_bill.py
+++ addons_yjzy/yjzy_extend/models/transport_bill.py
@@ -673,7 +673,7 @@ class transport_bill(models.Model):
 
     @api.depends('all_invoice_ids', 'all_invoice_ids.state', 'all_invoice_ids.residual_signed', 'sale_invoice_id',
                  'sale_invoice_id.state', 'sale_invoice_id.residual_signed', 'purchase_invoice_ids',
-                 'purchase_invoice_ids.state', )
+                 'purchase_invoice_ids.state')
     def compute_invoice_paid_state(self):
         for one in self:
             invoice_paid_state = 'e'
